c6644bf54baf64aa6ae61dff865801662b9237b6
suuuuu017 <suuuuu0177@outlook.com>
2021-09-18 15:54:59 +0800

    start working with parser

--- project/p1/main.c
+++ project/p1/main.c
@@ -7,21 +7,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(){
+    //exit flag
     int notExit = 1;
-    char s[1024];
-    char *string = s; // character array pointer
-    size_t size = 1024; // initial size of char array
-    char **string_pointer = &string; // double pointer to char array
+
+    //input line setup
+    char l[1024];
+    char *line = l;
+    size_t size = 1024;
+    char **line_pointer = &line;
+
+    //pid for executor
+    pid_t pid = 0;
+
     do{
+        //just exit special case
         printf("mumsh $ ");
-        size_t characters = getline(string_pointer,&size,stdin);
+        size_t characters = getline(line_pointer, &size, stdin);
 //        printf("The number of characters getline() read: %zu\n", characters);
-//        printf("You typed: %s\n",string);
-        if(strcmp(string, "exit\n") == 0){
+//        printf("You typed: %s\n",line);
+        if (strcmp(line, "exit\n") == 0){
             printf("mumsh $ exit\n");
             notExit = 0;
+        }
+        else if (strcmp(line, "ls\n") == 0){
+            char *cmd = "ls";
+            char *argv[3];
+            argv[0] = "ls";
+            argv[1] = "-la";
+            argv[2] = NULL;
+            execvp(cmd, argv);
         }
     } while (notExit);
 }
