fac513dd0301d563800b012f716497aefd39d998
suuuuu017 <suuuuu0177@outlook.com>
2021-10-14 11:47:33 +0800

    change

---
+++ cmake-build-debug/CMakeFiles/VE482_2021FA.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/suuuuu017/CLionProjects/VE482_2021FA/execute.c" "/Users/suuuuu017/CLionProjects/VE482_2021FA/cmake-build-debug/CMakeFiles/VE482_2021FA.dir/execute.c.o"
+  "/Users/suuuuu017/CLionProjects/VE482_2021FA/main.c" "/Users/suuuuu017/CLionProjects/VE482_2021FA/cmake-build-debug/CMakeFiles/VE482_2021FA.dir/main.c.o"
+  "/Users/suuuuu017/CLionProjects/VE482_2021FA/parser.c" "/Users/suuuuu017/CLionProjects/VE482_2021FA/cmake-build-debug/CMakeFiles/VE482_2021FA.dir/parser.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/suuuuu017/CLionProjects/VE482_2021FA/main.cpp" "/Users/suuuuu017/CLionProjects/VE482_2021FA/cmake-build-debug/CMakeFiles/VE482_2021FA.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/VE482_2021FA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VE482_2021FA.dir/execute.c.o"
+  "CMakeFiles/VE482_2021FA.dir/main.c.o"
+  "CMakeFiles/VE482_2021FA.dir/main.cpp.o"
+  "CMakeFiles/VE482_2021FA.dir/parser.c.o"
+  "VE482_2021FA"
+  "VE482_2021FA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/VE482_2021FA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
